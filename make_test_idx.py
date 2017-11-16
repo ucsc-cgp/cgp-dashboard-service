f90fa00c80538a084f1c1ee338b73ac71a4557a9
mjkrause <michael.j.krause@gmail.com>
2017-11-16 11:45:47 -0800

    insignificant: changed some var and def names

--- make_test_idx.py
+++ make_test_idx.py
@@ -12,55 +12,54 @@ def main():
     Create a copy of the two lines N times, where N is a number 
     > 9,999, and write into a new file.
     MK, 2017-11-14"""
-    
-    #print(uuid.uuid4())
+
+    # print(uuid.uuid4())
     fname = './test/fb_index.jsonl'
 
     data_in = []  # declare list
     data_in = read_jsonl(data_in, fname)
-    #print("First two lines: {}".format(data_in))
+    # print("First two lines: {}".format(data_in))
 
     parsed2 = json.loads(data_in.pop())  # load string method
     parsed1 = json.loads(data_in.pop())
 
     data_out = []
-    N = 3 #random.randint(9999, 1000001)
-    data_out = create_JSONL(data_out, parsed1, parsed2, N)
-    
+    n = 3  # random.randint(9999, 1000001)
+    create_jsonl(data_out, parsed1, parsed2, n)
+
     # Change values in JSON file:
-    #(parsed1, parsed2) = put_newvals(parsed1, parsed2)
+    # (parsed1, parsed2) = put_newvals(parsed1, parsed2)
 
     # Some form of pretty-print:
-    #print json.dumps(parsed1, indent=4, sort_keys=True)
-    #print json.dumps(parsed2, indent=4, sort_keys=True)
+    # print json.dumps(parsed1, indent=4, sort_keys=True)
+    # print json.dumps(parsed2, indent=4, sort_keys=True)
 
     print json.dumps(data_out, indent=4, sort_keys=True)
 
 
-def create_JSONL(data_out, parsed1, parsed2, N):
+def create_jsonl(data_out, parsed1, parsed2, n):
     """ """
     # Change values in JSON file:
     (parsed1, parsed2) = put_newvals(parsed1, parsed2)
+    i = 0
+    assert isinstance(n, object)
+    while i < n:
+        # New ID:
+        parsed1['index']['_id'] += 1
 
-    n = 0
-    while n < N:
-         # New ID: 
-         parsed1['index']['_id'] += 1
+        # Set new UUIDs:
+        parsed2['donor'] = str(uuid.uuid1())
+        parsed2['file_id'] = str(uuid.uuid1())
 
-         # Set new UUIDs:
-         parsed2['donor'] = str(uuid.uuid1())
-         parsed2['file_id'] = str(uuid.uuid1())
+        data_out.append(json.dumps(parsed1))
+        data_out.append(json.dumps(parsed2))
 
-         data_out.append(json.dumps(parsed1))
-         data_out.append(json.dumps(parsed2))
-
-         n += 1
+        i += 1
 
     return data_out
 
-         
-def put_newvals(parsed1, parsed2):
 
+def put_newvals(parsed1, parsed2):
     # New ID:
     parsed1['index']['_id'] += 1
 
@@ -70,16 +69,15 @@ def put_newvals(parsed1, parsed2):
 
     return parsed1, parsed2
 
-    
+
 def read_jsonl(data, fname):
     """Read first two lines into list data and return it."""
     with open(fname, 'r') as f:
-             data.append(''.join(f.readline()))
-             data.append(''.join(f.readline()))
+        data.append(''.join(f.readline()))
+        data.append(''.join(f.readline()))
     return data
 
 
 if __name__ == "__main__":
-    #print("   a random UUID:")
+    # print("   a random UUID:")
     main()
- 
